478a6f812e1e6a00060f06f08e6b9462e3110940
ikadgzl <ikadgzl@gmail.com>
2022-02-10 02:36:25 +0300

    implemented managin routing logic

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,37 @@
-import '../styles/globals.css'
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import { magic } from '../lib/magic';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [isLoading, setIsLoading] = useState(false);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const manageRouting = async () => {
+      const isLoggedIn = await magic.user.isLoggedIn();
+
+      if (isLoggedIn) {
+        router.push('/');
+      } else {
+        router.push('/login');
+      }
+    };
+
+    router.events.on('routeChangeComplete', () => setIsLoading(false));
+    router.events.on('routeChangeError', () => setIsLoading(false));
+
+    manageRouting();
+
+    return () => {
+      router.events.off('routeChangeComplete');
+      router.events.off('routeChangeError');
+    };
+  }, [router]);
+
+  //TODO: put loading
+  return isLoading ? 'loading..' : <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
